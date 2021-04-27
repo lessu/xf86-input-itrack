d1dea13b95bc41f80ed0e46523b82bf5039b7f9b
Limingyi <limingyi@example.com>
2021-04-27 13:02:36 +0800

    movment filter bug fix + noise filter

--- src/post_stage/movement.c
+++ src/post_stage/movement.c
@@ -22,8 +22,12 @@ void movement_clear(struct movement_s *movement){
 struct movement_action_s movement_add(struct movement_s *movement,const struct movement_action_s *act){
     // if cached action is too old,clear them
     if(movement->cached != NULL){
-        int diff = time_diff_ms(&movement->cached->action.time,&act->time);
-        if(diff < 20 * MOVEMENT_CACHE_COUNT){
+        struct movement_cached_s *p = movement->cached;
+        while( p->next != NULL ){
+            p = p->next;
+        }
+        int diff = time_diff_ms(&act->time,&p->action.time);
+        if(diff > 20 * MOVEMENT_CACHE_COUNT){
             movement_clear(movement);
         }
     }
@@ -47,21 +51,31 @@ struct movement_action_s movement_add(struct movement_s *movement,const struct m
     struct movement_cached_s *oldest = movement->cached;
     if(oldest != NULL){
 
+        new_act = oldest->action;
+
         /** if cached is full ,remove head one */
         if (cachedLength > MOVEMENT_CACHE_COUNT){
             movement->cached = movement->cached->next;
-        }
-    
-        new_act = oldest->action;
-
-        if (cachedLength > MOVEMENT_CACHE_COUNT){
             free(oldest);
         }
     }
 
     /** todo:// filter tiny movement */
+    // sum_all dx,dy
+    struct movement_cached_s *p = movement->cached;
+    float sum_dx = 0,sum_dy = 0;
+    while( p != NULL ){
+        sum_dx = sum_dx + p->action.dx;
+        sum_dy = sum_dy + p->action.dy;
+        p = p->next;
+    }
 
+    if((sum_dx < 2 && sum_dx>-2) && (sum_dy < 2 && sum_dy > -2)){
+        new_act.dx = 0;
+        new_act.dy = 0;
+    }
 
+    // todo:// slow down pointer move when dx/dy is small enough
     return new_act;
 
     // /** iterate all,make avg*/
